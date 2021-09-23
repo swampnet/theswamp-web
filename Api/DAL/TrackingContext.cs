7365a5f7839928614caa37421ee0e376a611511b
Pj <petejw@theswamp.co.uk>
2021-09-23 17:39:48 +0100

    hit DB for device values

---
+++ Api/DAL/TrackingContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSwamp.Api.DAL.Entities;
+
+namespace TheSwamp.Api.DAL
+{
+    public class TrackingContext : DbContext
+    {
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<DeviceValue> DeviceValues { get; set; }
+
+        public TrackingContext(DbContextOptions<TrackingContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("trk");
+
+            modelBuilder.Entity<Device>().ToTable("Device");
+            modelBuilder.Entity<DeviceValue>().ToTable("DeviceValue");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Api/Services/MonitorService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSwamp.Api.DAL;
+using TheSwamp.Api.Interfaces;
+using TheSwamp.Shared;
+
+namespace TheSwamp.Api.Services
+{
+    internal class MonitorService : IMonitor
+    {
+        private readonly TrackingContext _trackingContext;
+
+        public MonitorService(TrackingContext trackingContext)
+        {
+            _trackingContext = trackingContext;
+        }
+
+        public async Task<Device[]> GetDevicesAsync()
+        {
+            var x = await _trackingContext.Devices.ToArrayAsync();
+
+            return x.Select(y => new Device() { 
+                Id = y.Id,
+                Name = y.Name,
+                Description = y.Description,
+                CreatedOnUtc = y.CreatedOnUtc
+            }).ToArray();
+        }
+
+
+        public async Task<DeviceValue[]> GetValuesAsync (int deviceId)
+        {
+            var x = await _trackingContext.DeviceValues
+                .Where(dv => dv.DeviceId == deviceId)
+                .ToArrayAsync();
+
+            return x.Select(y => new DeviceValue()
+            {
+                TimestampUtc = y.TimestampUtc,
+                Value = y.Value
+            }).ToArray();
+        }
+    }
+}

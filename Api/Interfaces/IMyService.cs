8c6e795cd6b24302809458862a3e9bf8b7d67cd9
Pj <petejw@theswamp.co.uk>
2021-09-23 12:48:55 +0100

    add di stuff to api

---
+++ Api/Interfaces/IMyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSwamp.Api.Interfaces
+{
+    public interface IMyService
+    {
+        string Boosh();
+    }
+}

b1346d021c09c1053e005d963ed570c3ebda4059
Pj <petejw@theswamp.co.uk>
2021-09-26 21:13:07 +0100

    Add PI agent project (just stubbed out)

---
+++ Agent/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using TheSwamp.Shared;
+
+namespace Agent
+{
+    internal class Program
+    {
+        private static IConfiguration _cfg;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            API.Initialise(_cfg["api:endpoint"], _cfg["api:key"]);
+
+
+        }
+    }
+}

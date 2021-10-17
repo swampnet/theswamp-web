1eb4d4f826e68872bc7633a8dd650112b8c5b486
Pj <petejw@theswamp.co.uk>
2021-10-17 10:39:38 +0100

    SignalR hub RandD - Using a timer function to trigger it so this aint gonna work on my hosting plan

---
+++ Api/HubFunction.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace TheSwamp.Api
+{
+    public class HubFunction
+    {
+        [FunctionName("negotiate")]
+        public SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "serverlessSample")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        [FunctionName("broadcast")]
+        public static async Task Broadcast([TimerTrigger("0 * * * * *")] TimerInfo myTimer,
+        [SignalR(HubName = "serverlessSample")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            await signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "tick",
+                    Arguments = new[] { $"{DateTime.Now}" }
+                });
+        }
+    }
+}

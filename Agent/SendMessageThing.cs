cf930f749db6172142c83f0a1cf28a23440481f2
pj <pete.whitby@octavian.co.uk>
2021-10-20 14:04:40 +0100

    grab time messages from cfg & pick a random one each time

--- Agent/SendMessageThing.cs
+++ Agent/SendMessageThing.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Threading.Tasks;
 using TheSwamp.Shared;
+using Microsoft.Extensions.Configuration;
 
 namespace Agent
 {
     public class SendMessageThing : IThing
     {
         public TimeSpan PollInterval => TimeSpan.FromMinutes(1);
+        private readonly Random _rnd = new Random();
 
         public async Task PollAsync(Monitor monitor)
         {
             if(DateTime.Now.Minute == 0)
             {
-                Console.WriteLine($"{this.GetType()} Ding Dong!");
+                var x = Program.Cfg.GetSection("time-quotes").Get<string[]>();
+
+                var msg = x[_rnd.Next(0, x.Length - 1)];
+                msg = msg.Replace("{{now}}", DateTime.Now.ToShortTimeString());
+
+                Console.WriteLine($"{this.GetType()} {msg}");
 
                 await API.PostMessageAsync(new AgentMessage()
                 {
                     Type = "led-matrix",
                     Properties = new System.Collections.Generic.List<Property>() {
-                        new Property("content", $"Ding Dong! It's {DateTime.Now.ToShortTimeString()}!")
+                        new Property("content", msg)
                     }
                 });
             }

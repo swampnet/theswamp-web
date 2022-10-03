fb419a611b5835626719d59cb4238656356967b0
Aime Johnson <petejw@theswamp.co.uk>
2022-10-03 16:02:58 +0100

    Add noddy LWIN import app

--- ImportLWIN/Program.cs
+++ ImportLWIN/Program.cs
@@ -1,10 +1,71 @@
-﻿namespace ImportLWIN
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Bibliography;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Data;
+using Microsoft.Extensions.DependencyInjection;
+using ImportLWIN.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using EFCore.BulkExtensions;
+
+namespace ImportLWIN
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                IConfiguration config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var builder = new ServiceCollection()
+                    .AddSingleton<Program>()
+                    .AddSingleton<IConfiguration>(config)
+                    .AddDbContext<LWINContext>(options => options.UseSqlServer(config.GetConnectionString("connectionstring.swampnet")))
+                    .BuildServiceProvider();
+
+                var app = builder.GetRequiredService<Program>();
+
+                await app.RunAsync(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private readonly LWINContext _context;
+
+        public Program(LWINContext context)
+        {
+            _context = context;
+        }
+
+
+        private async Task RunAsync(string[] args)
+        {
+            var raw = LoadSourceData()
+                .Tables["LWINdatabase"]
+                .ToRaw();
+
+            await _context.BulkInsertAsync(raw);
+        }
+
+
+        private DataSet LoadSourceData()
+        {
+            // Load spreadsheet
+            // data\\LWIN-20221002.xlsx
+            var path = "data\\LWIN-20221002.xlsx";
+
+            Console.WriteLine($"Loading source data: {path}");
+
+            using (var xls = new XLWorkbook(path))
+            {
+                return xls.ToDataSet();
+            }
         }
     }
 }

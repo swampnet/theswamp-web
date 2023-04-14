c73a7b7462af15b5777f90c9c9491d1273c5b53c
pj <pete.whitby@octavian.co.uk>
2023-04-14 17:14:55 +0100

    add emoji & apathetic personalities

--- Api/WineFunction.cs
+++ Api/WineFunction.cs
@@ -163,6 +163,14 @@ namespace TheSwamp.Api
                 case ReviewPersonality.Drunk:
                     seed.Add(new ChatMessage(StaticValues.ChatMessageRoles.System, "I also want you to act as a drunk person. You will only answer like a very drunk person texting and nothing else. Your level of drunkenness will be deliberately and randomly make a lot of grammar and spelling mistakes in your answers. You will also randomly ignore what I said and say something random with the same level of drunkeness I mentioned."));
                     break;
+
+                case ReviewPersonality.Emoji:
+                    seed.Add(new ChatMessage(StaticValues.ChatMessageRoles.System, "I also want you to just use emojis in all responses."));
+                    break;
+
+                case ReviewPersonality.Apathetic:
+                    seed.Add(new ChatMessage(StaticValues.ChatMessageRoles.System, "I also want you to be completely apathetic and uninterested in your response, you don't reallyh care for wine and you're reluctantly being forced to drink and review this muck."));
+                    break;
             }
 
             return seed;
